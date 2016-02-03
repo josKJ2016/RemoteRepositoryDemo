7f8051d87faac5092ac5588bbb9817affee4bdbb
KJ <josKJ2016@outlook.com>
2016-02-03 10:21:08 +0800

    第一次commit

---
+++ Project/src/test.java
@@ -0,0 +1,17 @@
+import java.io.Console;
+import java.util.HashMap;
+import java.util.Map;
+
+import _11_room.model.RoomVO;
+
+public class test {
+
+    public static void main(String[] args) {
+    	int x=3;
+    	int[] a ={1,2,x};
+    	String[] b={"a","b","c"};
+    	
+    	System.out.println(a[2]);
+    }
+
+}
